40bd9958c25c83df1d2315a9f967104472f927ed
michi-b <michi.s.brunner@gmail.com>
2022-05-25 21:30:03 +0200

    add enum generator to test source generation

---
+++ MsbRps.CodeAnalysis/EnumGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace MsbRps.CodeAnalysis
+{
+    [Generator]
+    public class EnumGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            context.RegisterPostInitializationOutput
+            (
+                ctx => ctx.AddSource("EnumExtensionsAttribute.g.cs", SourceText.From(SourceGenerationHelper.Attribute, Encoding.UTF8))
+            );
+        }
+    }
+
+    public static class SourceGenerationHelper
+    {
+        public const string Attribute = @"
+namespace NetEscapades.EnumGenerators
+{
+    [System.AttributeUsage(System.AttributeTargets.Enum)]
+    public class EnumExtensionsAttribute : System.Attribute
+    {
+    }
+}";
+    }
+}
--- MsbRpsConsoleTest/Program.cs
+++ MsbRpsConsoleTest/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using NetEscapades.EnumGenerators;
+
 BufferedStream stream = new BufferedStream(new MemoryStream());
+
+[EnumExtensions]
+public enum TestEnum
+{
+    Test1,
+    Test2,
+}
