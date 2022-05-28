afded4be17e40afa3eca7c2925884d6aa8322c12
michi-b <michi.s.brunner@gmail.com>
2022-05-28 23:01:19 +0200

    successfully publish nuget package

---
+++ MsbRps.CodeAnalysis/DummySourceGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace DummySourceGenerator
+{
+    [Generator]
+    public class DummySourceGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context) { }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.AddSource("DummyGenerated.cs", SourceText.From(@"class DummyGenerated{}", Encoding.UTF8));
+        }
+    }
+}
