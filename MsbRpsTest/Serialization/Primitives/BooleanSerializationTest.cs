df0b563425ed33c7c505e3374e54578bddedb28b
michi-b <michi.s.brunner@gmail.com>
2022-05-31 22:18:18 +0200

    add some primitives serialization code and tests

---
+++ MsbRpsTest/Serialization/Primitives/BooleanSerializationTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MsbRps.Serialization;
+
+namespace MsbRpsTest.Serialization.Primitives;
+
+[TestClass]
+[SuppressMessage("ReSharper", "BuiltInTypeReferenceStyle")]
+public class BooleanSerializationTest : SerializationTest<Boolean>
+{
+    protected override int BufferSize { get; } = sizeof(Boolean);
+
+    [TestMethod]
+    public void PreservesTrue()
+    {
+        TestPreserves(true);
+    }
+
+    [TestMethod]
+    public void PreservesFalse()
+    {
+        TestPreserves(false);
+    }
+
+    protected override void Write(Boolean value, byte[] buffer)
+    {
+        buffer.Write(value);
+    }
+
+    protected override Boolean Read(byte[] buffer) => buffer.ReadBoolean();
+}
---
+++ MsbRpsTest/Serialization/Primitives/Int32SerializationTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MsbRps.Serialization;
+
+namespace MsbRpsTest.Serialization.Primitives;
+
+[TestClass]
+[SuppressMessage("ReSharper", "BuiltInTypeReferenceStyle")]
+[SuppressMessage("ReSharper", "BuiltInTypeReferenceStyleForMemberAccess")]
+public class Int32SerializationTest : SerializationTest<Int32>
+{
+    protected override int BufferSize => sizeof(Int32);
+
+    [TestMethod]
+    public void PreservesZero()
+    {
+        TestPreserves(0);
+    }
+
+    [TestMethod]
+    public void PreservesOne()
+    {
+        TestPreserves(1);
+    }
+
+    [TestMethod]
+    public void PreservesMinusOne()
+    {
+        TestPreserves(-1);
+    }
+
+    [TestMethod]
+    public void PreservesMinusZero()
+    {
+        TestPreserves(-0);
+    }
+
+    [TestMethod]
+    public void PreservesMinimum()
+    {
+        TestPreserves(Int32.MinValue);
+    }
+
+    [TestMethod]
+    public void PreservesMaximum()
+    {
+        TestPreserves(Int32.MaxValue);
+    }
+
+    protected override void Write(Int32 value, byte[] buffer)
+    {
+        buffer.Write(value);
+    }
+
+    protected override int Read(byte[] buffer) => buffer.ReadInt32();
+}
