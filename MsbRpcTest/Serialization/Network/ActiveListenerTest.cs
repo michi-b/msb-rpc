9c257ee489077691ff0ac2d99f073bb3ffbead6b
misbat <michi.s.brunner@gmail.com>
2022-10-16 08:21:11 +0200

    add active listener tests

---
+++ MsbRpcTest/Serialization/Network/ActiveListenerTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MsbRpcTest.Serialization.Network.Listeners;
+
+namespace MsbRpcTest.Serialization.Network;
+
+[TestClass]
+public class ActiveListenerTest : ListenerTest
+{
+    [TestMethod]
+    public async Task ClosingConnectionStopsListening()
+    {
+        await TestClosingConnectionStopsListening();
+    }
+
+    [TestMethod]
+    public async Task SingleByteMessagesIsDelivered()
+    {
+        await TestSingleByteMessagesIsDelivered();
+    }
+
+    [TestMethod]
+    public async Task EmptyMessageIsDelivered()
+    {
+        await TestEmptyMessageIsDelivered();
+    }
+
+    [TestMethod]
+    public async Task IntMessageIsDelivered()
+    {
+        await TestIntMessageIsDelivered();
+    }
+
+    protected override SingleConnectionListener.GetReceiveMessagesTask CreateGetReceiveMessagesTask() => NetworkUtility.ReceiveMessagesActiveAsync;
+}
