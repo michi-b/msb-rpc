ee84412740f0c0c5908d8befb38ec97862f14b6f
misbat <misbat.dev@gmail.com>
2022-12-04 17:56:45 +0100

    start off some work on the rpc generator

---
+++ MsbRpc.Generator/Filters.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MsbRpc.Generator;
+
+public static class Filters
+{
+    public static bool IsInterfaceDeclarationWithAttributes(SyntaxNode node, CancellationToken cancellationToken)
+        => (node as InterfaceDeclarationSyntax)?.AttributeLists.Count > 0;
+}
--- MsbRpc.Generator/RpcGenerator.cs
+++ MsbRpc.Generator/RpcGenerator.cs
@@ -1,6 +1,7 @@
 ﻿//todo: implement
 
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace MsbRpc.Generator;
 
@@ -9,6 +10,35 @@ public class RpcGenerator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        throw new NotImplementedException();
+        IncrementalValuesProvider<InterfaceDeclarationSyntax> rpcContracts
+            = context.SyntaxProvider.CreateSyntaxProvider
+                (
+                    GetIsAttributedSyntax,
+                    GetRpcContract
+                )
+                .Where(syntax => syntax != null)!;
+        
+        //need to register codegen to have the filters run
+        context.RegisterSourceOutput(rpcContracts, Execute);
+    }
+
+    private static void Execute(SourceProductionContext context, InterfaceDeclarationSyntax rpcContract)
+    {
+        string name = rpcContract.Identifier.Text;
+        Console.WriteLine($"Generating {name}");
+    }
+
+    private bool GetIsAttributedSyntax(SyntaxNode syntaxNode, CancellationToken cancellationToken)
+    {
+        // ReSharper disable once UsePatternMatching
+        // prefer this separate for debugging
+        var interfaceDeclarationSyntax = syntaxNode as InterfaceDeclarationSyntax;
+        return interfaceDeclarationSyntax != null && interfaceDeclarationSyntax.AttributeLists.Any();
+    }
+
+    private static InterfaceDeclarationSyntax? GetRpcContract(GeneratorSyntaxContext context, CancellationToken cancellationToken)
+    {
+        context.SemanticModel.GetSymbolInfo(context.Node, cancellationToken);
+        return context.Node as InterfaceDeclarationSyntax;
     }
 }
