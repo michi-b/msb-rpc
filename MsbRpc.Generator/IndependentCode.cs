6c7b7da516d81aff167b0df23c632bb32540c3be
misbat <misbat.dev@gmail.com>
2022-12-30 14:51:04 +0100

    auto-format

--- MsbRpc.Generator/IndependentCode.cs
+++ MsbRpc.Generator/IndependentCode.cs
@@ -6,6 +6,6 @@ internal static class IndependentCode
 {
     private const string ArgumentsBufferWrite = Variables.ArgumentsWriter + "." + Methods.BufferWrite;
     public const string EmptyBufferExpression = Types.BufferUtility + "." + Fields.BufferEmpty;
-    
+
     public static string GetBufferWrite(string variableName) => $"{ArgumentsBufferWrite}({variableName});";
 }
--- MsbRpc.Test.Generator/Incrementer/Implementation/Incrementer.cs
+++ MsbRpc.Test.Generator/Incrementer/Implementation/Incrementer.cs
@@ -5,9 +5,9 @@ namespace MsbRpc.Test.Generator.Incrementer.Implementation;
 internal class Incrementer : IIncrementerServerImplementation
 {
     private int _stored;
-    
+
     public int Increment(int value) => value + 1;
-    
+
     public void Store(int value)
     {
         _stored = value;
