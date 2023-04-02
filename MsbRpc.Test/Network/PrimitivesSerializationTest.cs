33610ccb9dcb834b00a4953edfd24df69f55e2f3
misbat <misbat.dev@gmail.com>
2023-04-02 17:42:42 +0200

    implement nullable primitive serialization

--- MsbRpc.Test/Network/PrimitivesSerializationTest.cs
+++ MsbRpc.Test/Network/PrimitivesSerializationTest.cs
@@ -64,7 +64,47 @@ public class PrimitivesSerializationTest : Test
         const UInt16 uint16Value = 32143;
         const UInt32 uint32Value = 3245324423;
         const UInt64 uint64Value = UInt64.MaxValue;
-
+        
+        //bool?
+        bool? nullBooleanValue = null;
+        bool? nullableTrueBooleanValue = true;
+        //byte?
+        byte? nullByteValue = null;
+        byte? nullableByteValue = 10;
+        //sbyte?
+        sbyte? nullSByteValue = null;
+        sbyte? nullableSByteValue = -123;
+        //char?
+        char? nullCharValue = null;
+        char? nullableCharValue = 'b';
+        //short?
+        short? nullInt16Value = null;
+        short? nullableInt16Value = -22345;
+        //ushort?
+        ushort? nullUInt16Value = null;
+        ushort? nullableUInt16Value = 65432;
+        //int?
+        int? nullInt32Value = null;
+        int? nullableInt32Value = 123454789;
+        //uint?
+        uint? nullUInt32Value = null;
+        uint? nullableUInt32Value = 1234567890;
+        //long?
+        long? nullInt64Value = null;
+        long? nullableInt64Value = -1234567890123456789;
+        //ulong?
+        ulong? nullUInt64Value = null;
+        ulong? nullableUInt64Value = 12345678901234567890;
+        //float?
+        float? nullSingleValue = null;
+        float? nullableSingleValue = 123.456f;
+        //double?
+        double? nullDoubleValue = null;
+        double? nullableDoubleValue = 111.222;
+        //decimal?
+        decimal? nullDecimalValue = null;
+        decimal? nullableDecimalValue = 654.321m;
+        
         int offset = 0;
 
         buffer.WriteBool(trueBooleanValue, offset);
@@ -110,7 +150,85 @@ public class PrimitivesSerializationTest : Test
 
         buffer.WriteUlong(uint64Value, offset);
         offset += PrimitiveSerializer.UlongSize;
+        
+        buffer.WriteBoolNullable(nullBooleanValue, offset);
+        offset += PrimitiveSerializer.NullableBoolSize;
+        
+        buffer.WriteBoolNullable(nullableTrueBooleanValue, offset);
+        offset += PrimitiveSerializer.NullableBoolSize;
 
+        buffer.WriteByteNullable(nullByteValue, offset);
+        offset += PrimitiveSerializer.NullableByteSize;
+        
+        buffer.WriteByteNullable(nullableByteValue, offset);
+        offset += PrimitiveSerializer.NullableByteSize;
+        
+        buffer.WriteSbyteNullable(nullSByteValue, offset);
+        offset += PrimitiveSerializer.NullableSbyteSize;
+        
+        buffer.WriteSbyteNullable(nullableSByteValue, offset);
+        offset += PrimitiveSerializer.NullableSbyteSize;
+        
+        buffer.WriteCharNullable(nullCharValue, offset);
+        offset += PrimitiveSerializer.NullableCharSize;
+        
+        buffer.WriteCharNullable(nullableCharValue, offset);
+        offset += PrimitiveSerializer.NullableCharSize;
+        
+        buffer.WriteShortNullable(nullInt16Value, offset);
+        offset += PrimitiveSerializer.NullableShortSize;
+        
+        buffer.WriteShortNullable(nullableInt16Value, offset);
+        offset += PrimitiveSerializer.NullableShortSize;
+
+        buffer.WriteUshortNullable(nullUInt16Value, offset);
+        offset += PrimitiveSerializer.NullableUshortSize;
+        
+        buffer.WriteUshortNullable(nullableUInt16Value, offset);
+        offset += PrimitiveSerializer.NullableUshortSize;
+        
+        buffer.WriteIntNullable(nullInt32Value, offset);
+        offset += PrimitiveSerializer.NullableIntSize;
+        
+        buffer.WriteIntNullable(nullableInt32Value, offset);
+        offset += PrimitiveSerializer.NullableIntSize;
+        
+        buffer.WriteUintNullable(nullUInt32Value, offset);
+        offset += PrimitiveSerializer.NullableUintSize;
+        
+        buffer.WriteUintNullable(nullableUInt32Value, offset);
+        offset += PrimitiveSerializer.NullableUintSize;
+        
+        buffer.WriteLongNullable(nullInt64Value, offset);
+        offset += PrimitiveSerializer.NullableLongSize;
+        
+        buffer.WriteLongNullable(nullableInt64Value, offset);
+        offset += PrimitiveSerializer.NullableLongSize;
+        
+        buffer.WriteUlongNullable(nullUInt64Value, offset);
+        offset += PrimitiveSerializer.NullableUlongSize;
+        
+        buffer.WriteUlongNullable(nullableUInt64Value, offset);
+        offset += PrimitiveSerializer.NullableUlongSize;
+        
+        buffer.WriteFloatNullable(nullSingleValue, offset);
+        offset += PrimitiveSerializer.NullableFloatSize;
+        
+        buffer.WriteFloatNullable(nullableSingleValue, offset);
+        offset += PrimitiveSerializer.NullableFloatSize;
+        
+        buffer.WriteDoubleNullable(nullDoubleValue, offset);
+        offset += PrimitiveSerializer.NullableDoubleSize;
+        
+        buffer.WriteDoubleNullable(nullableDoubleValue, offset);
+        offset += PrimitiveSerializer.NullableDoubleSize;
+        
+        buffer.WriteDecimalNullable(nullDecimalValue, offset);
+        offset += PrimitiveSerializer.NullableDecimalSize;
+        
+        buffer.WriteDecimalNullable(nullableDecimalValue, offset);
+        offset += PrimitiveSerializer.NullableDecimalSize;
+        
         int byteCount = offset;
 
         // ReSharper disable once ConditionIsAlwaysTrueOrFalse
@@ -171,6 +289,84 @@ public class PrimitivesSerializationTest : Test
 
         UInt64 uint64Result = bytes.ReadUlong(offset);
         offset += PrimitiveSerializer.UlongSize;
+        
+        bool? nullBooleanResult = bytes.ReadBoolNullable(offset);
+        offset += PrimitiveSerializer.NullableBoolSize;
+        
+        bool? nullableTrueBooleanResult = bytes.ReadBoolNullable(offset);
+        offset += PrimitiveSerializer.NullableBoolSize;
+        
+        byte? nullByteResult = bytes.ReadByteNullable(offset);
+        offset += PrimitiveSerializer.NullableByteSize;
+        
+        byte? nullableByteResult = bytes.ReadByteNullable(offset);
+        offset += PrimitiveSerializer.NullableByteSize;
+        
+        sbyte? nullSByteResult = bytes.ReadSbyteNullable(offset);
+        offset += PrimitiveSerializer.NullableSbyteSize;
+        
+        sbyte? nullableSByteResult = bytes.ReadSbyteNullable(offset);
+        offset += PrimitiveSerializer.NullableSbyteSize;
+        
+        char? nullCharResult = bytes.ReadCharNullable(offset);
+        offset += PrimitiveSerializer.NullableCharSize;
+        
+        char? nullableCharResult = bytes.ReadCharNullable(offset);
+        offset += PrimitiveSerializer.NullableCharSize;
+        
+        short? nullInt16Result = bytes.ReadShortNullable(offset);
+        offset += PrimitiveSerializer.NullableShortSize;
+        
+        short? nullableInt16Result = bytes.ReadShortNullable(offset);
+        offset += PrimitiveSerializer.NullableShortSize;
+        
+        ushort? nullUInt16Result = bytes.ReadUshortNullable(offset);
+        offset += PrimitiveSerializer.NullableUshortSize;
+        
+        ushort? nullableUInt16Result = bytes.ReadUshortNullable(offset);
+        offset += PrimitiveSerializer.NullableUshortSize;
+        
+        int? nullInt32Result = bytes.ReadIntNullable(offset);
+        offset += PrimitiveSerializer.NullableIntSize;
+        
+        int? nullableInt32Result = bytes.ReadIntNullable(offset);
+        offset += PrimitiveSerializer.NullableIntSize;
+        
+        uint? nullUInt32Result = bytes.ReadUintNullable(offset);
+        offset += PrimitiveSerializer.NullableUintSize;
+        
+        uint? nullableUInt32Result = bytes.ReadUintNullable(offset);
+        offset += PrimitiveSerializer.NullableUintSize;
+        
+        long? nullInt64Result = bytes.ReadLongNullable(offset);
+        offset += PrimitiveSerializer.NullableLongSize;
+        
+        long? nullableInt64Result = bytes.ReadLongNullable(offset);
+        offset += PrimitiveSerializer.NullableLongSize;
+        
+        ulong? nullUInt64Result = bytes.ReadUlongNullable(offset);
+        offset += PrimitiveSerializer.NullableUlongSize;
+        
+        ulong? nullableUInt64Result = bytes.ReadUlongNullable(offset);
+        offset += PrimitiveSerializer.NullableUlongSize;
+        
+        float? nullSingleResult = bytes.ReadFloatNullable(offset);
+        offset += PrimitiveSerializer.NullableFloatSize;
+        
+        float? nullableSingleResult = bytes.ReadFloatNullable(offset);
+        offset += PrimitiveSerializer.NullableFloatSize;
+        
+        double? nullDoubleResult = bytes.ReadDoubleNullable(offset);
+        offset += PrimitiveSerializer.NullableDoubleSize;
+        
+        double? nullableDoubleResult = bytes.ReadDoubleNullable(offset);
+        offset += PrimitiveSerializer.NullableDoubleSize;
+        
+        decimal? nullDecimalResult = bytes.ReadDecimalNullable(offset);
+        offset += PrimitiveSerializer.NullableDecimalSize;
+        
+        decimal? nullableDecimalResult = bytes.ReadDecimalNullable(offset);
+        offset += PrimitiveSerializer.NullableDecimalSize;
 
         Assert.AreEqual(byteCount, offset);
 
@@ -189,5 +385,31 @@ public class PrimitivesSerializationTest : Test
         Assert.AreEqual(uint16Value, uint16Result);
         Assert.AreEqual(uint32Value, uint32Result);
         Assert.AreEqual(uint64Value, uint64Result);
+        Assert.AreEqual(nullBooleanValue, nullBooleanResult);
+        Assert.AreEqual(nullableTrueBooleanValue, nullableTrueBooleanResult);
+        Assert.AreEqual(nullByteValue, nullByteResult);
+        Assert.AreEqual(nullableByteValue, nullableByteResult);
+        Assert.AreEqual(nullSByteValue, nullSByteResult);
+        Assert.AreEqual(nullableSByteValue, nullableSByteResult);
+        Assert.AreEqual(nullCharValue, nullCharResult);
+        Assert.AreEqual(nullableCharValue, nullableCharResult);
+        Assert.AreEqual(nullInt16Value, nullInt16Result);
+        Assert.AreEqual(nullableInt16Value, nullableInt16Result);
+        Assert.AreEqual(nullUInt16Value, nullUInt16Result);
+        Assert.AreEqual(nullableUInt16Value, nullableUInt16Result);
+        Assert.AreEqual(nullInt32Value, nullInt32Result);
+        Assert.AreEqual(nullableInt32Value, nullableInt32Result);
+        Assert.AreEqual(nullUInt32Value, nullUInt32Result);
+        Assert.AreEqual(nullableUInt32Value, nullableUInt32Result);
+        Assert.AreEqual(nullInt64Value, nullInt64Result);
+        Assert.AreEqual(nullableInt64Value, nullableInt64Result);
+        Assert.AreEqual(nullUInt64Value, nullUInt64Result);
+        Assert.AreEqual(nullableUInt64Value, nullableUInt64Result);
+        Assert.AreEqual(nullSingleValue, nullSingleResult);
+        Assert.AreEqual(nullableSingleValue, nullableSingleResult);
+        Assert.AreEqual(nullDoubleValue, nullDoubleResult);
+        Assert.AreEqual(nullableDoubleValue, nullableDoubleResult);
+        Assert.AreEqual(nullDecimalValue, nullDecimalResult);
+        Assert.AreEqual(nullableDecimalValue, nullableDecimalResult);
     }
 }
--- MsbRpc/Serialization/Primitives/PrimitiveSerializer.cs
+++ MsbRpc/Serialization/Primitives/PrimitiveSerializer.cs
@@ -5,6 +5,8 @@ namespace MsbRpc.Serialization.Primitives;
 
 public static partial class PrimitiveSerializer
 {
+    private const int NullableSizeOverhead = 1;
+
     /// <summary>
     ///     shortcut access for <see cref="BitConverter" />.IsLittleEndian
     /// </summary>
@@ -29,6 +31,30 @@ public static partial class PrimitiveSerializer
             PrimitiveType.UInt64 => UlongSize,
             _ => throw new ArgumentOutOfRangeException(nameof(primitiveType), primitiveType, null)
         };
+    }
+
+    private static void WriteNullable<T>(this byte[] buffer, T? value, int offset, Action<byte[], T, int> writeNonNullable)
+        where T : struct
+    {
+        if (value == null)
+        {
+            buffer[offset] = 0;
+            return;
+        }
+
+        buffer[offset] = 1;
+        writeNonNullable(buffer, value.Value, offset + 1);
+    }
+
+    private static T? ReadNullable<T>(this byte[] buffer, int offset, Func<byte[], int, T> readNonNullable)
+        where T : struct
+    {
+        if (buffer[offset] == 0)
+        {
+            return null;
+        }
+
+        return readNonNullable(buffer, offset + 1);
     }
 
     #region Byte
@@ -202,6 +228,183 @@ public static partial class PrimitiveSerializer
     }
 
     [PublicAPI] public const int DecimalSize = 16;
+
+    #endregion
+
+    #region Byte?
+
+    public static void WriteByteNullable(this byte[] buffer, byte? value, int offset = 0)
+    {
+        buffer.WriteNullable(value, offset, WriteByte);
+    }
+
+    public static byte? ReadByteNullable(this byte[] buffer, int offset = 0) => buffer.ReadNullable(offset, ReadByte);
+
+    [PublicAPI] public const int NullableByteSize = ByteSize + NullableSizeOverhead;
+
+    #endregion
+
+    #region SByte?
+
+    public static void WriteSbyteNullable(this byte[] buffer, sbyte? value, int offset = 0)
+    {
+        buffer.WriteNullable(value, offset, WriteSbyte);
+    }
+
+    public static sbyte? ReadSbyteNullable(this byte[] buffer, int offset = 0) => buffer.ReadNullable(offset, ReadSbyte);
+
+    [PublicAPI] public const int NullableSbyteSize = SbyteSize + NullableSizeOverhead;
+
+    #endregion
+
+    #region Bool?
+
+    public static void WriteBoolNullable(this byte[] buffer, bool? value, int offset = 0)
+    {
+        buffer.WriteNullable(value, offset, WriteBool);
+    }
+
+    public static bool? ReadBoolNullable(this byte[] buffer, int offset = 0)
+    {
+        if (buffer[offset] == 0)
+        {
+            return null;
+        }
+
+        return buffer.ReadBool(offset + 1);
+    }
+
+    [PublicAPI] public const int NullableBoolSize = 2;
+
+    #endregion
+
+    #region Char?
+
+    public static void WriteCharNullable(this byte[] buffer, char? value, int offset = 0)
+    {
+        buffer.WriteNullable(value, offset, WriteChar);
+    }
+
+    public static char? ReadCharNullable(this byte[] buffer, int offset = 0) => buffer.ReadNullable(offset, ReadChar);
+
+    [PublicAPI] public const int NullableCharSize = CharSize + NullableSizeOverhead;
+
+    #endregion
+
+    #region Int?
+
+    public static void WriteIntNullable(this byte[] buffer, int? value, int offset = 0)
+    {
+        buffer.WriteNullable(value, offset, WriteInt);
+    }
+
+    public static int? ReadIntNullable(this byte[] buffer, int offset = 0) => buffer.ReadNullable(offset, ReadInt);
+
+    [PublicAPI] public const int NullableIntSize = IntSize + NullableSizeOverhead;
+
+    #endregion
+
+    #region Long?
+
+    public static void WriteLongNullable(this byte[] buffer, long? value, int offset = 0)
+    {
+        buffer.WriteNullable(value, offset, WriteLong);
+    }
+
+    public static long? ReadLongNullable(this byte[] buffer, int offset = 0) => buffer.ReadNullable(offset, ReadLong);
+
+    [PublicAPI] public const int NullableLongSize = LongSize + NullableSizeOverhead;
+
+    #endregion
+
+    #region Short?
+
+    public static void WriteShortNullable(this byte[] buffer, short? value, int offset = 0)
+    {
+        buffer.WriteNullable(value, offset, WriteShort);
+    }
+
+    public static short? ReadShortNullable(this byte[] buffer, int offset = 0) => buffer.ReadNullable(offset, ReadShort);
+
+    [PublicAPI] public const int NullableShortSize = ShortSize + NullableSizeOverhead;
+
+    #endregion
+
+    #region UInt?
+
+    public static void WriteUintNullable(this byte[] buffer, uint? value, int offset = 0)
+    {
+        buffer.WriteNullable(value, offset, WriteUint);
+    }
+
+    public static uint? ReadUintNullable(this byte[] buffer, int offset = 0) => buffer.ReadNullable(offset, ReadUint);
+
+    [PublicAPI] public const int NullableUintSize = UintSize + NullableSizeOverhead;
+
+    #endregion
+
+    #region ULong?
+
+    public static void WriteUlongNullable(this byte[] buffer, ulong? value, int offset = 0)
+    {
+        buffer.WriteNullable(value, offset, WriteUlong);
+    }
+
+    public static ulong? ReadUlongNullable(this byte[] buffer, int offset = 0) => buffer.ReadNullable(offset, ReadUlong);
+
+    [PublicAPI] public const int NullableUlongSize = UlongSize + NullableSizeOverhead;
+
+    #endregion
+
+    #region UShort?
+
+    public static void WriteUshortNullable(this byte[] buffer, ushort? value, int offset = 0)
+    {
+        buffer.WriteNullable(value, offset, WriteUshort);
+    }
+
+    public static ushort? ReadUshortNullable(this byte[] buffer, int offset = 0) => buffer.ReadNullable(offset, ReadUshort);
+
+    [PublicAPI] public const int NullableUshortSize = UshortSize + NullableSizeOverhead;
+
+    #endregion
+
+    #region Float?
+
+    public static void WriteFloatNullable(this byte[] buffer, float? value, int offset = 0)
+    {
+        buffer.WriteNullable(value, offset, WriteFloat);
+    }
+
+    public static float? ReadFloatNullable(this byte[] buffer, int offset = 0) => buffer.ReadNullable(offset, ReadFloat);
+
+    [PublicAPI] public const int NullableFloatSize = FloatSize + NullableSizeOverhead;
+
+    #endregion
+
+    #region Double?
+
+    public static void WriteDoubleNullable(this byte[] buffer, double? value, int offset = 0)
+    {
+        buffer.WriteNullable(value, offset, WriteDouble);
+    }
+
+    public static double? ReadDoubleNullable(this byte[] buffer, int offset = 0) => buffer.ReadNullable(offset, ReadDouble);
+
+    [PublicAPI] public const int NullableDoubleSize = DoubleSize + NullableSizeOverhead;
+
+    #endregion
+
+    #region Decimal?
+
+    public static void WriteDecimalNullable(this byte[] buffer, decimal? value, int offset = 0)
+    {
+        buffer.WriteNullable(value, offset, WriteDecimal);
+    }
+
+    public static decimal? ReadDecimalNullable(this byte[] buffer, int offset = 0) => buffer.ReadNullable(offset, ReadDecimal);
+
+    [PublicAPI] public const int NullableDecimalSize = DecimalSize + NullableSizeOverhead;
 
     #endregion
 }
