871a1164776423600cc7c00c8d0c9e9bec42d52e
misbat <misbat.dev@gmail.com>
2023-05-08 20:21:17 +0200

    finish custom serialization writing

--- MsbRpc.Generator/Serialization/Serializations/CustomSerialization.cs
+++ MsbRpc.Generator/Serialization/Serializations/CustomSerialization.cs
@@ -5,7 +5,7 @@ using MsbRpc.Generator.Info;
 
 namespace MsbRpc.Generator.Serialization.Serializations;
 
-public class CustomSerialization : ISerialization
+public sealed class CustomSerialization : ISerialization
 {
     public static readonly KeyValuePair<TypeReferenceInfo, CustomSerializationInfo>[] EmptyArray
         = Array.Empty<KeyValuePair<TypeReferenceInfo, CustomSerializationInfo>>();
@@ -14,25 +14,36 @@ public class CustomSerialization : ISerialization
     private readonly string _deserializationMethod;
     private readonly string _serializationMethod;
     private readonly string _sizeMember;
-
+    private readonly bool _isConstantSize;
+    
     public CustomSerialization(TypeReferenceInfo targetType, CustomSerializationInfo serializationInfo)
     {
         _declarationSyntax = targetType.GetDeclarationSyntax();
         string serializerName = serializationInfo.SerializerTypeReference.Declaration.Name;
+        _isConstantSize = serializationInfo.Kind == CustomSerializerKind.ConstantSize;
         _sizeMember = serializerName + "." + serializationInfo.SizeMemberName;
         _serializationMethod = serializerName + "." + serializationInfo.SerializationMethodName;
         _deserializationMethod = serializerName + "." + serializationInfo.DeserializationMethodName;
     }
 
-    public void WriteSizeExpression(IndentedTextWriter writer, string targetExpression) { }
+    public void WriteSizeExpression(IndentedTextWriter writer, string targetExpression)
+    {
+        writer.Write(_isConstantSize ? _sizeMember : $"{_sizeMember}({targetExpression})");
+    }
 
-    public void WriteSerializationStatement(IndentedTextWriter writer, string bufferWriterExpression, string valueExpression) { }
+    public void WriteSerializationStatement(IndentedTextWriter writer, string bufferWriterExpression, string valueExpression)
+    {
+        writer.Write($"{_serializationMethod}({bufferWriterExpression}, {valueExpression})");
+    }
 
-    public void WriteDeserializationExpression(IndentedTextWriter writer, string bufferReaderExpression) { }
+    public void WriteDeserializationExpression(IndentedTextWriter writer, string bufferReaderExpression)
+    {
+        writer.Write($"{_deserializationMethod}({bufferReaderExpression})");
+    }
 
     public bool GetIsVoid() => false;
 
-    public virtual bool GetIsResolved() => true;
+    public bool GetIsResolved() => true;
 
     public string GetDeclarationSyntax() => _declarationSyntax;
 }
